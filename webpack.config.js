9ab961c02bb93098eea863266bd5478600e06d3c
GSouzaT <tsleirbag@gmail.com>
2024-01-21 16:04:44 -0300

    fix: process not defined error

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { webpack } = require("webpack");
 
 module.exports = {
-    entry: "./app/index.jsx",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index_bundle.js"
-    },
-    module: {
-        rules: [
-            { test: /\.(js|jsx)$/, use: "babel-loader" },
-            { test: /\.css$/, use: ["style-loader", "css-loader"] }
-        ]
-    },
-    resolve: {
-        extensions: [".jsx", "..."]
-    },
-    mode: "development",
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "app/index.html"
-        })
-    ]
-}
+  entry: "./app/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  resolve: {
+    extensions: [".jsx", "..."],
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "app/index.html",
+    }),
+    new Dotenv(),
+  ],
+};
