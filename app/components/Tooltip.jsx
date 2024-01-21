5f56a6457efa57ac4fb90535b0b360ff57c981c4
GSouzaT <tsleirbag@gmail.com>
2024-01-21 20:09:23 -0300

    refactor: change HOC to render props

--- app/components/Tooltip.jsx
+++ app/components/Tooltip.jsx
@@ -1,28 +1,28 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import withHover from "./withHover";
+import Hover from "./Hover";
 
 const container = {
   position: "relative",
   display: "flex",
 };
 
-class Tooltip extends React.Component {
-  render() {
-    const { element, children, hovering } = this.props;
-
-    return (
-      <div style={container}>
-        {hovering === true && element}
-        {children}
-      </div>
-    );
-  }
+export default function Tooltip({ children, element }) {
+  return (
+    <Hover>
+      {(hovering) => {
+        return (
+          <div style={container}>
+            {hovering === true && element}
+            {children}
+          </div>
+        );
+      }}
+    </Hover>
+  );
 }
 
 Tooltip.PropTypes = {
   children: PropTypes.node.isRequired,
   element: PropTypes.node.isRequired,
 };
-
-export default withHover(Tooltip);
