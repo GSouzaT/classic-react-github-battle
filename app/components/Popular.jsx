056c83ff9a07af1b39e6a404980e8a118b3cd137
GSouzaT <tsleirbag@gmail.com>
2024-01-24 21:39:39 -0300

    migrate components to class fields

--- app/components/Popular.jsx
+++ app/components/Popular.jsx
@@ -26,23 +26,17 @@ LanguagesNav.PropTypes = {
 };
 
 export default class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedLanguage: "All",
-      repos: null,
-      error: null,
-    };
-
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
+  state = {
+    selectedLanguage: "All",
+    repos: null,
+    error: null,
+  };
 
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage);
   }
 
-  updateLanguage(selectedLanguage) {
+  updateLanguage = (selectedLanguage) => {
     this.setState({
       selectedLanguage,
       error: null,
@@ -62,7 +56,7 @@ export default class Popular extends React.Component {
           error,
         });
       });
-  }
+  };
   render() {
     const { selectedLanguage, repos, error } = this.state;
 
